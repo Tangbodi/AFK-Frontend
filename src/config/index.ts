59c17a6aa762241abc8ef684efc768bba9d68465
Ethan <yeshuangdong@sina.com>
2023-09-06 23:33:45 +0800

    logout debug

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 export const settingsList = [
   { name:'My info', path: '/settings/myinfo'},
   { name:'Notifications', path: '/settings/notifications'},
-  { name:'Activities', path: '/settings/activities'},
+  // { name:'Activities', path: '/settings/activities'},
   { name:'Privacy and Security', path: '/settings/security'}
 ]
 
